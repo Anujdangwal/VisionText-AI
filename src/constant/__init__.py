b44119ba73b06c0724006e41112b1e17ed973517
anuj dangwal <anujdangwal20@gmail.com>
2025-07-18 17:03:25 +0530

    push

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,9 +1,6 @@
 import os
 
 root = os.getcwd()
-# Define the base artifact folder
-ARTIFACT_FOLDER = os.path.join(root,"artifacts")
-
 
 # Example
 checkpoint_path = os.path.join(root , "model_checkpoint", "best_model.h5")
