4e84aa07e5df112f5c094fd75154a6ffa2fb8957
anuj dangwal <anujdangwal20@gmail.com>
2025-07-18 11:09:30 +0530

    few changes:

--- app.py
+++ app.py
@@ -181,4 +181,5 @@ def image_captioning_page():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
